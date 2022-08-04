80a5b37357cb0b9d73c0fbc9626ecbc642dd282e
Aleksander Valchev <97479750+AxelRood204@users.noreply.github.com>
2022-08-04 20:13:32 +0300

    Add files via upload

---
+++ src/services/data.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+const baseUrl = 'https://quantum-server-app.herokuapp.com/dev/testdata/?'
+
+const getUpTo100 = () => {
+    const request = axios.get(`https://quantum-server-app.herokuapp.com/dev/testdata/?set=1dsine&start=0&end=10&dx=1`)
+
+    return request.then(response => response.data)
+}
+
+const getWanted = (set, start, end, dx) => {
+    const request = axios.get(`${baseUrl}set=${set}dsine&start=${start}&end=${end}&dx=${dx}`)
+
+    return request.then(response => response.data)
+}
+
+const dataService = {
+    getUpTo100,
+    getWanted
+}
+
+export default dataService
