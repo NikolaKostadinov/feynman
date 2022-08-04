fe115fd53aa0ce558326ca7d34541c65c5158882
Aleksander Valchev <97479750+AxelRood204@users.noreply.github.com>
2022-08-04 21:13:26 +0300

    Update SideBarDiv.js

--- client/src/components/SideBarDiv.js
+++ client/src/components/SideBarDiv.js
@@ -2,7 +2,7 @@ const SideBarDiv = ({ srcPath, id, alt, text }) => {
     return (
         <div className="sideBarDiv" id={id}>
           <div>
-            <img src={require(`../images/${srcPath}`)} alt={alt} width='300' height="300"/>
+            <img src={require(`../images/${srcPath}`)} alt={alt} />
             <p>{text}</p>
           </div>
         </div>
